f1b37bc04f48c48b8b921b44d6426c4fce408622
shruthik shetty <shrutikshetty14@gmail.com>
2025-09-06 23:00:43 +0530

    fix things in Date rage filter

--- src/components/date-filter.tsx
+++ src/components/date-filter.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React from 'react';
+import React, { useState } from 'react';
 import { Popover, PopoverContent, PopoverTrigger } from './ui/popover';
 import { Button } from './ui/button';
 import { Calendar } from './ui/calendar';
@@ -22,7 +22,10 @@ const DateFilter = ({
   value: DateState | undefined;
   setValue: (value: DateState | undefined) => void;
 }) => {
-  const [dateType, setDateType] = React.useState<'from' | 'to'>('from');
+  // store the if user is selecting from or to date
+  const [dateType, setDateType] = useState<'from' | 'to'>('from');
+  //state to handle calendar visibility
+  const [openCalendar, setOpenCalendar] = useState(false);
 
   //handle Value Select
   const handleValueChange = (date: Date | undefined) => {
@@ -98,42 +101,44 @@ const DateFilter = ({
         <PopoverClose className="absolute top-3 right-3">
           <X className="hover:text-base-white text-ui-400 h-5 w-5" />
         </PopoverClose>
-        {/* Inner content */}
-        <Popover>
-          <p>{filters.label}</p>
-          {/* "From" and "To" Buttons */}
-          <div className="flex flex-row items-center justify-center gap-5">
-            <div>
-              <p className="text-ui-400 mb-1 text-xs">From</p>
-              <PopoverTrigger asChild>
-                <Button
-                  variant="outline"
-                  className="border-ui-600"
-                  onClick={() => {
-                    setDateType('from');
-                  }}
-                >
-                  {value?.gte ? value.gte.toDateString() : 'Pick Date'}
-                </Button>
-              </PopoverTrigger>
-            </div>
-            <Minus className="text-ui-400 h-6 w-6" />
-            <div>
-              <p className="text-ui-400 mb-1 text-xs">To</p>
-              <PopoverTrigger asChild>
-                <Button
-                  variant="outline"
-                  className="border-ui-600"
-                  onClick={() => {
-                    setDateType('to');
-                  }}
-                >
-                  {value?.lte ? value.lte.toDateString() : 'Pick Date'}
-                </Button>
-              </PopoverTrigger>
-            </div>
+
+        <p>{filters.label}</p>
+        {/* "From" and "To" Buttons */}
+        <div className="flex flex-row items-center justify-center gap-5">
+          <div>
+            <p className="text-ui-400 mb-1 text-xs">From</p>
+
+            <Button
+              variant="outline"
+              className="border-ui-600"
+              onClick={() => {
+                setOpenCalendar(true);
+                setDateType('from');
+              }}
+            >
+              {value?.gte ? value.gte.toDateString() : 'Pick Date'}
+            </Button>
           </div>
-          {/* Calender */}
+          <Minus className="text-ui-400 h-6 w-6" />
+          <div>
+            <p className="text-ui-400 mb-1 text-xs">To</p>
+
+            <Button
+              variant="outline"
+              className="border-ui-600"
+              onClick={() => {
+                setOpenCalendar(true);
+                setDateType('to');
+              }}
+            >
+              {value?.lte ? value.lte.toDateString() : 'Pick Date'}
+            </Button>
+          </div>
+        </div>
+        {/* Calender */}
+        <Popover open={openCalendar} onOpenChange={setOpenCalendar}>
+          {/* This is a "dummy" trigger for positioning; it won't be visible */}
+          <PopoverTrigger></PopoverTrigger>
           <PopoverContent className="bg-base-black w-auto">
             <Calendar
               mode="single"
@@ -143,12 +148,17 @@ const DateFilter = ({
               className="text-base-white"
               buttonVariant={'outline'}
               disabled={disableDates}
+              modifiersClassNames={{
+                pinned: 'border-2 border-brand-500 rounded-md',
+              }}
               modifiers={{
                 pinned: pinnedDate ? [pinnedDate] : [],
               }}
               classNames={{
                 years_dropdown: 'text-base-black',
                 months_dropdown: 'text-base-black',
+                day_button:
+                  'data-[selected-single=true]:bg-brand-500 data-[selected-single=true]:border-0',
               }}
             />
             {/* buttons for calender */}
