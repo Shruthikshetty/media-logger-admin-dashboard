c3ccf8ca235bab644735c4b335dc347fff2956a5
shruthik shetty <shrutikshetty14@gmail.com>
2025-09-12 22:28:53 +0530

    handle file uplaod

--- src/app/(tabs)/games/page.tsx
+++ src/app/(tabs)/games/page.tsx
@@ -1,11 +1,16 @@
+'use client';
 import React from 'react';
 import JsonImporter from '~/components/json-importer';
 
 //@todo screen in progress
 const GamesTab = () => {
   return (
-    <div className="text-feedback-success-light text-3xl p-5">
-      <JsonImporter />
+    <div className="text-feedback-success-light p-5 text-3xl">
+      <JsonImporter
+        onSuccess={(file) => {
+          console.log('success');
+        }}
+      />
     </div>
   );
 };
--- src/components/json-importer.tsx
+++ src/components/json-importer.tsx
@@ -8,17 +8,75 @@ import { Button } from './ui/button';
 import scrollStyles from '~/css-modules/scrollbar.module.css';
 import { cn } from '~/lib/utils';
 import { CODE_AREA_PLACEHOLDER_EXAMPLE } from '~/constants/screen.constants';
+import { toast } from 'sonner';
 
-const JsonImporter = () => {
+const JsonImporter = ({
+  onSuccess,
+  fileName = 'imported.json',
+}: {
+  onSuccess: (file: File) => void;
+  fileName?: string;
+}) => {
+  // store json text
   const [jsonString, setJsonString] = useState('');
   //store the input ref
   const inputRef = useRef<HTMLInputElement>(null);
+
+  /**
+   * Handles file change event on input element.
+   * If a file is selected, it reads the file as text and updates the json string state.
+   */
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      //set the json on load
+      reader.onload = () => {
+        setJsonString(reader.result as string);
+      };
+      reader.readAsText(file);
+    }
+  };
+
+  /**
+   * This function is called when the user confirms the import.
+   * this will convert the json string to a json file and return the file as a callback
+   */
+  const handleConfirm = () => {
+    try {
+      // parse json
+      const data = JSON.parse(jsonString);
+      //@todo  validate json to match the schema
+      const blob = new Blob([jsonString], { type: 'application/json' });
+      const jsonFile = new File([blob], fileName, { type: 'application/json' });
+      // return the file on success
+      onSuccess(jsonFile);
+    } catch (e) {
+      console.log(e);
+      toast.error('Failed to import json , check your json format', {
+        classNames: {
+          toast: '!bg-feedback-error',
+        },
+      });
+    }
+  };
+
   return (
     <div className="text-base-white flex flex-col gap-3">
       <div className="flex flex-col gap-2">
         <Label className="text-base">Upload JSON File</Label>
         {/* hidden file input */}
-        <input type="file" accept=".json" ref={inputRef} hidden />
+        <input
+          type="file"
+          accept=".json"
+          ref={inputRef}
+          hidden
+          onClick={(e: React.MouseEvent<HTMLInputElement>) =>
+            //reset the input value
+            ((e.target as HTMLInputElement).value = '')
+          }
+          onChange={handleFileChange}
+        />
         <Button
           variant={'outline'}
           type="button"
@@ -44,7 +102,12 @@ const JsonImporter = () => {
           placeholder={CODE_AREA_PLACEHOLDER_EXAMPLE}
         />
       </div>
-      <Button variant={'blue'} type="button">
+      <Button
+        variant={'blue'}
+        type="button"
+        onClick={handleConfirm}
+        disabled={!jsonString}
+      >
         Confirm Import
       </Button>
     </div>
