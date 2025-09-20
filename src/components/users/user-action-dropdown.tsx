01868eaeda8be68aeee47ee09b9a24bf283fd674
shruthik shetty <shrutikshetty14@gmail.com>
2025-09-20 18:16:32 +0530

    add actions user component

--- src/components/users/user-action-dropdown.tsx
+++ src/components/users/user-action-dropdown.tsx
@@ -1,4 +1,4 @@
-import { Ellipsis } from 'lucide-react';
+import { Crown, Ellipsis, Eye, SquarePen, Trash2 } from 'lucide-react';
 import React from 'react';
 import {
   DropdownMenu,
@@ -7,20 +7,49 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from '../ui/dropdown-menu';
+import { cn } from '~/lib/utils';
 
-//@TODO in progress
 /**
  * This component is used to display the actions for a user
  * Mainly used in user table performs actions like view details , delete , edit role
  * @param movieId
  */
-const UserActionsDropdown = ({}: { userId: string }) => {
+const UserActionsDropdown = ({ userId }: { userId: string }) => {
+  //user action items
+  const userActionItems = [
+    {
+      title: 'View profile',
+      icon: Eye,
+      color: 'text-base-white',
+      onClick: () => {}, //@TODO
+    },
+    {
+      title: 'Edit User',
+      icon: SquarePen,
+      color: 'text-base-white',
+      onClick: () => {}, //@TODO
+    },
+    {
+      title: 'Change Role',
+      icon: Crown,
+      color: 'text-base-white',
+      onClick: () => {}, //@TODO
+    },
+    {
+      title: 'Delete User',
+      icon: Trash2,
+      color: 'text-feedback-error',
+      onClick: () => {}, //@TODO
+    },
+  ];
   return (
     <DropdownMenu>
       <DropdownMenuTrigger
+        disabled={!userId}
         aria-label="Open user actions"
         type="button"
         className="hover:bg-ui-600 rounded-md p-1"
+        onClick={(e) => e.stopPropagation()}
       >
         <Ellipsis />
       </DropdownMenuTrigger>
@@ -29,12 +58,20 @@ const UserActionsDropdown = ({}: { userId: string }) => {
         align="center"
       >
         <DropdownMenuGroup>
-          <DropdownMenuItem>
-            <p>Add</p>
-          </DropdownMenuItem>
-          <DropdownMenuItem>
-            <p>edit</p>
-          </DropdownMenuItem>
+          {userActionItems.map((item) => (
+            <DropdownMenuItem
+              disabled={!userId}
+              key={item.title}
+              className={cn(
+                `hover:bg-ui-800 focus:bg-ui-800 focus:${item.color} text-md flex flex-row gap-2 rounded-md p-2`,
+                item.color,
+              )}
+              onClick={item.onClick}
+            >
+              <item.icon className={cn('h-5 w-5', item.color)} />
+              <p>{item.title}</p>
+            </DropdownMenuItem>
+          ))}
         </DropdownMenuGroup>
       </DropdownMenuContent>
     </DropdownMenu>
