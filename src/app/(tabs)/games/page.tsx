1cd3682867bf6cfe060650c83726718f5893546f
shruthik shetty <shrutikshetty14@gmail.com>
2025-09-24 21:56:36 +0530

    add mock games tab

--- src/app/(tabs)/games/page.tsx
+++ src/app/(tabs)/games/page.tsx
@@ -1,10 +1,17 @@
 'use client';
 import { Plus, Search, Upload } from 'lucide-react';
 import React from 'react';
+import GamesTable from '~/components/games/games-table';
 import MediaFilters from '~/components/media-filters';
 import TitleSubtitle from '~/components/title-subtitle';
 import { Button } from '~/components/ui/button';
-import { Card, CardDescription, CardHeader } from '~/components/ui/card';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '~/components/ui/card';
 import { Input } from '~/components/ui/input';
 import { GamesFilterConfig } from '~/constants/config.constants';
 
@@ -72,6 +79,20 @@ const GamesTab = () => {
             />
           </div>
         </CardHeader>
+      </Card>
+      {/* Games data and table */}
+      <Card className="border-ui-600 text-base-white from-base-black to-ui-900 bg-gradient-to-r">
+        <CardHeader>
+          <CardTitle className="text-xl font-semibold">
+            Game Directory (100 games)
+          </CardTitle>
+          <CardDescription className="text-ui-400 text-sm">
+            Complete list of games in your collection
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <GamesTable />
+        </CardContent>
       </Card>
     </div>
   );
---
+++ src/components/games/games-table.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+//@TODO inprogress
+const GamesTable = () => {
+  return <div>GamesTable Component</div>;
+};
+
+export default GamesTable;
