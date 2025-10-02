263e0008d3654a5bc1979c60eb93dee27928c8ec
shruthik shetty <shrutikshetty14@gmail.com>
2025-10-02 18:06:21 +0530

    update attributions

--- src/components/attributions.tsx
+++ src/components/attributions.tsx
@@ -7,23 +7,58 @@ import Image from 'next/image';
  */
 const Attributions = () => {
   return (
-    <div className="flex flex-col items-center">
-      <p className="text-ui-400 mb-2 text-xs">
-        This product uses the TMDB API but is not endorsed or certified by TMDB.{' '}
-        <br />
+    <div className="flex flex-col items-center space-y-4">
+      {/* Single Attribution Text */}
+      <p className="text-ui-400 mb-2 text-center text-xs">
+        This product uses data and images from <b>TMDB</b>, <b>IGDB</b>, and{' '}
+        <b>RAWG</b>. We are not endorsed or certified by these providers. Logos
+        are trademarks of their respective owners.
       </p>
-      <a
-        href="https://www.themoviedb.org/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <Image
-          src="/tmdb-attribution.svg"
-          alt="The Movie Database API Logo"
-          width={100}
-          height={20}
-        />
-      </a>
+      {/* Logo Row */}
+      <div className="flex flex-row items-center space-x-4">
+        {/* TMDB Logo */}
+        <a
+          href="https://www.themoviedb.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="The Movie Database"
+        >
+          <Image
+            src="/tmdb-attribution.svg"
+            alt="TMDB Logo"
+            width={100}
+            height={20}
+          />
+        </a>
+        {/* IGDB Logo */}
+        <a
+          href="https://www.igdb.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="IGDB"
+        >
+          <Image
+            src="/igdb-attribution.svg"
+            alt="IGDB Logo"
+            width={100}
+            height={24}
+          />
+        </a>
+        {/* RAWG "Logo" (as bold text or use your own SVG/Png) */}
+        <a
+          href="https://www.igdb.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="IGDB"
+        >
+          <Image
+            src="/rawg-attribution.svg"
+            alt="RAWG Logo"
+            width={100}
+            height={24}
+          />
+        </a>
+      </div>
     </div>
   );
 };
