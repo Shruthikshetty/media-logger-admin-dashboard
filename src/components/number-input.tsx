f0b6dcaac0d924cf25fd39f1873e346d703e9da6
shruthik shetty <shrutikshetty14@gmail.com>
2025-10-03 15:03:30 +0530

    fix comments

--- src/components/number-input.tsx
+++ src/components/number-input.tsx
@@ -4,7 +4,7 @@ import { Input } from './ui/input';
 
 /**
  * A wrapper around the `Input` component that allows only numbers.
- * onChanges the value to a number and passes it to the `onChange` prop. 
+ * onChanges the value to a number and passes it to the `onChange` prop.
  * @param {number | undefined} value The current value of the input.
  * @param {(val: number | undefined) => void} onChange Called whenever the value changes.
  * @param {Omit<ComponentProps<typeof Input>, 'type' | 'onChange'>} [rest] Any other props to be passed to `Input`.
@@ -15,12 +15,14 @@ const NumberInput = ({
   onChange,
   ...rest
 }: Omit<ComponentProps<typeof Input>, 'type' | 'onChange'> & {
-  onChange: (val: number | undefined) => void;
+  onChange: (val: number | undefined | null) => void;
 }) => {
   return (
     <Input
-      value={value === undefined || value === 0 ? '' : value}
-      onChange={(e) => onChange(Number(e.target.value))}
+      value={value === null || value === undefined ? '' : value}
+      onChange={(e) =>
+        onChange(e.target.value === '' ? null : Number(e.target.value))
+      }
       type="number"
       {...rest}
     />
