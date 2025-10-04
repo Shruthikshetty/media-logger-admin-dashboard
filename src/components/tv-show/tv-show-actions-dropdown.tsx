a45572c3ddc013add1103e6e19da9776b8e1baeb
shruthik shetty <shrutikshetty14@gmail.com>
2025-10-04 21:48:56 +0530

    add columns for tv show

---
+++ src/components/tv-show/tv-show-actions-dropdown.tsx
@@ -0,0 +1,83 @@
+import { TvShowBase } from '~/services/tv-show-service';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuPortal,
+  DropdownMenuTrigger,
+} from '../ui/dropdown-menu';
+import { Ellipsis, Eye, SquarePen, Trash2 } from 'lucide-react';
+import { cn } from '~/lib/utils';
+
+/**
+ * This component is used to display the actions for a Tv show
+ * Mainly used in Tv show table performs actions like view details , edit , delete
+ * @param data [TvShowBase data]
+ */
+const TvShowActionDropdown = ({ data }: { data: TvShowBase }) => {
+  // tv show dropdown action items
+  const tvShowActionItems = [
+    {
+      title: 'View Details',
+      icon: Eye,
+      color: 'text-base-white',
+      onClick: () => {
+        // @TODO
+      },
+    },
+    {
+      title: 'Edit Tv Show',
+      icon: SquarePen,
+      color: 'text-base-white',
+      onClick: () => {
+        // @TODO
+      },
+    },
+    {
+      title: 'Delete Movie',
+      icon: Trash2,
+      color: 'text-feedback-error',
+      onClick: () => {
+        // @TODO
+      },
+    },
+  ];
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger
+        aria-label="Open movie actions"
+        type="button"
+        className="hover:bg-ui-600 rounded-md p-1"
+        onClick={(e) => e.stopPropagation()}
+      >
+        <Ellipsis />
+      </DropdownMenuTrigger>
+      <DropdownMenuPortal>
+        <DropdownMenuContent
+          className="bg-base-black border-ui-600 shadow-ui-600 text-base-white z-60 mx-5 my-2 rounded-lg border p-1 pb-1 shadow-[0px_0px_15px]"
+          align="center"
+        >
+          <DropdownMenuGroup>
+            {tvShowActionItems.map((item) => (
+              <DropdownMenuItem
+                disabled={!data._id}
+                key={item.title}
+                className={cn(
+                  `hover:bg-ui-800 focus:bg-ui-800 focus:${item.color} text-md flex flex-row gap-2 rounded-md p-2`,
+                  item.color,
+                )}
+                onClick={item.onClick}
+              >
+                <item.icon className={cn('h-5 w-5', item.color)} />
+                {item.title}
+              </DropdownMenuItem>
+            ))}
+          </DropdownMenuGroup>
+        </DropdownMenuContent>
+      </DropdownMenuPortal>
+    </DropdownMenu>
+  );
+};
+
+export default TvShowActionDropdown;
