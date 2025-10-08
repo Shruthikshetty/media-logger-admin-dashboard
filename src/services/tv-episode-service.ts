79df518c51191de4dd896d627c4c522d1570eb9c
shruthik shetty <shrutikshetty14@gmail.com>
2025-10-08 22:29:01 +0530

    add episode services

--- src/services/tv-episode-service.ts
+++ src/services/tv-episode-service.ts
@@ -2,7 +2,7 @@
  * @file contains the tv episode related services
  */
 
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { SeasonBase } from './tv-season-service';
 import { TvShowBase } from './tv-show-service';
 import { QueryKeys } from '~/constants/query-key.constants';
@@ -38,9 +38,46 @@ export type EpisodeFull = {
   season: Omit<SeasonBase, 'tvShow'> & { tvShow: TvShowBase };
 };
 
+export type AddEpisode = {
+  title: string;
+  description: string;
+  episodeNumber: number;
+  releaseDate: string;
+  runTime: number;
+  stillUrl: string;
+  season: string;
+};
+
+export type UpdateEpisode = Partial<{
+  title: string;
+  description: string;
+  episodeNumber: number;
+  releaseDate: string;
+  runTime: number;
+  stillUrl: string;
+  averageRating: number;
+}>;
+
 export type FetchEpisodeDetailsResponse<T> = {
   success: boolean;
   data: T extends true ? EpisodeFull : EpisodeBase;
+};
+
+type AddEpisodeResponse = {
+  success: boolean;
+  data: EpisodeBase;
+  message?: string;
+};
+
+type UpdateEpisodeResponse = {
+  success: boolean;
+  data: EpisodeBase;
+  message?: string;
+};
+
+type UpdateEpisodeRequest = {
+  updatedEpisode: UpdateEpisode;
+  episodeId: string;
 };
 
 //custom hook to fetch the episode details by id
@@ -63,6 +100,66 @@ export const useFetchEpisodeDetailsById = <T extends boolean = false>(
             signal,
           },
         )
+        .then((res) => res.data),
+  });
+};
+
+//custom hook to add a new episode by providing season id
+export const useAddEpisode = () => {
+  // initialize query client
+  const queryClient = useQueryClient();
+  return useMutation<AddEpisodeResponse, AxiosError<ApiError>, AddEpisode>({
+    mutationKey: [QueryKeys.addEpisode],
+    mutationFn: (episode: AddEpisode) => {
+      return apiClient
+        .post(Endpoints.baseEpisode, episode)
+        .then((res) => res.data);
+    },
+    onSuccess: () => {
+      // invalidate the query after adding an episode
+      queryClient.invalidateQueries({
+        queryKey: [QueryKeys.fetchTvShowByFilter],
+      });
+    },
+  });
+};
+
+//custom hook to delete a episode by id
+export const useDeleteEpisodeById = () => {
+  // initialize query client
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationKey: [QueryKeys.deleteEpisode],
+    mutationFn: (episodeId: string) =>
+      apiClient
+        .delete(Endpoints.baseEpisode + `/${episodeId}`)
+        .then((res) => res.data),
+    onSuccess: () => {
+      // invalidate the query after deleting an episode
+      queryClient.invalidateQueries({
+        queryKey: [QueryKeys.fetchTvShowByFilter],
+      });
+    },
+  });
+};
+
+//custom hook to edit a episode by id
+export const useUpdateEpisodeById = () => {
+  return useMutation<
+    UpdateEpisodeResponse,
+    AxiosError<ApiError>,
+    UpdateEpisodeRequest
+  >({
+    mutationKey: [QueryKeys.editEpisode],
+    mutationFn: ({
+      episodeId,
+      updatedEpisode,
+    }: {
+      episodeId: string;
+      updatedEpisode: UpdateEpisode;
+    }) =>
+      apiClient
+        .patch(Endpoints.baseEpisode + `/${episodeId}`, updatedEpisode)
         .then((res) => res.data),
   });
 };
