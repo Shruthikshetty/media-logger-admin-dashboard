56307ce3ae17d2bc72ce269d2ccdec24e76ee5aa
shruthik shetty <shrutikshetty14@gmail.com>
2025-10-08 19:31:12 +0530

    add content to episode details screen

--- src/app/(tabs)/tv-shows/episode/[episodeId]/page.tsx
+++ src/app/(tabs)/tv-shows/episode/[episodeId]/page.tsx
@@ -1,10 +1,11 @@
 'use client';
-import { Star } from 'lucide-react';
+import { PenSquare, Star, Trash2 } from 'lucide-react';
 import moment from 'moment';
 import { useParams } from 'next/navigation';
 import React from 'react';
 import BackButton from '~/components/back-button';
 import CustomImage from '~/components/custom-image';
+import { Button } from '~/components/ui/button';
 import { Card, CardContent } from '~/components/ui/card';
 import { useFetchEpisodeDetailsById } from '~/services/tv-episode-service';
 
@@ -18,7 +19,7 @@ const EpisodeDetails = () => {
   // get episode id from params
   const episodeId = useParams().episodeId as string;
   // fetch episode details
-  const { data } = useFetchEpisodeDetailsById(episodeId);
+  const { data, isLoading } = useFetchEpisodeDetailsById(episodeId);
   console.log(episodeId);
   return (
     <div className="flex h-full w-full flex-col gap-5 p-5">
@@ -28,35 +29,64 @@ const EpisodeDetails = () => {
           <p className="text-xl font-bold">
             #{data?.data?.episodeNumber}: {data?.data?.title}
           </p>
-          <CustomImage
-            alt="tv show poster"
-            src={data?.data.stillUrl}
-            width={255}
-            height={170}
-            className="aspect-[3/2] rounded-lg"
-            placeHolderClassname="aspect-[3/2] px-4 [&_[data-slot='icon']]:text-ui-400"
-          />
-          <p>
-            <span className="text-ui-400">Air Date:</span>{' '}
-            {data?.data?.releaseDate
-              ? moment(data?.data?.releaseDate).format('LL')
-              : '???'}
-          </p>
-          <p>
-            <span className="text-ui-400">Duration:</span>{' '}
-            {data?.data?.runTime ? data?.data?.runTime : '???'} min
-          </p>
-          <p className="flex flex-row gap-1">
-            <span className="text-ui-400">Rating:</span>{' '}
-            {data?.data.averageRating ? (
-              <span className="flex items-center gap-1">
-                {data?.data.averageRating}{' '}
-                <Star className="text-feedback-warning h-4 w-4" />
-              </span>
-            ) : (
-              '???'
-            )}
-          </p>
+          <div className="flex flex-col gap-2 sm:flex-row sm:justify-between">
+            <div className="flex flex-col gap-2">
+              <CustomImage
+                alt="tv show poster"
+                src={data?.data.stillUrl}
+                width={255}
+                height={170}
+                className="aspect-[3/2] rounded-lg"
+                placeHolderClassname="aspect-[3/2] px-4 [&_[data-slot='icon']]:text-ui-400"
+              />
+              <p>
+                <span className="text-ui-400">Air Date:</span>{' '}
+                {data?.data?.releaseDate
+                  ? moment(data?.data?.releaseDate).format('LL')
+                  : '???'}
+              </p>
+              <p>
+                <span className="text-ui-400">Duration:</span>{' '}
+                {data?.data?.runTime ? data?.data?.runTime : '???'} min
+              </p>
+              <p className="flex flex-row gap-1">
+                <span className="text-ui-400">Rating:</span>{' '}
+                {data?.data.averageRating ? (
+                  <span className="flex items-center gap-1">
+                    {data?.data.averageRating}{' '}
+                    <Star className="text-feedback-warning h-4 w-4" />
+                  </span>
+                ) : (
+                  '???'
+                )}
+              </p>
+            </div>
+            {/* Buttons */}
+            <div className="flex flex-row gap-2 sm:flex-col sm:gap-3">
+              <Button
+                variant={'blue'}
+                disabled={isLoading}
+                className="flex-1 sm:flex-none"
+                type="button"
+                aria-label="Edit Tv show"
+              >
+                <PenSquare />
+                Edit Episode
+              </Button>
+
+              <Button
+                variant={'red'}
+                className="text-base-white flex-1 sm:flex-none"
+                type="button"
+                aria-label="Delete Tv show"
+                disabled={isLoading}
+              >
+                <Trash2 />
+                Delete Episode
+              </Button>
+            </div>
+          </div>
+          <p className="text-xl font-semibold sm:hidden">Overview</p>
           <p className="text-ui-400 text-lg">{data?.data?.description}</p>
         </CardContent>
       </Card>
