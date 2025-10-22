cd85d8dd70199130d1863120b23710108af5f534
shruthik shetty <shrutikshetty14@gmail.com>
2025-10-22 23:47:35 +0530

    add test for spinner overlay

---
+++ src/components/__test__/app-spinner.test.tsx
@@ -0,0 +1,38 @@
+import { expect, it, describe, vi } from 'vitest';
+import { screen, render } from '@testing-library/react';
+import { SpinnerOverlay } from '../app-spinner';
+
+// mock the spinner store
+const spinnerState = {
+  showSpinner: true,
+  spinnerOptions: {
+    opacity: 60,
+    type: 'normal',
+  },
+};
+
+vi.mock('~/state-management/spinner-store', () => ({
+  _esModule: true,
+  useSpinnerStore: () => spinnerState,
+}));
+
+describe('app spinner component test cases', () => {
+  it('should render the app spinner overlay component', () => {
+    render(<SpinnerOverlay />);
+    expect(screen.getByLabelText('spinner-loader')).toBeInTheDocument();
+  });
+
+  it('should render properly with dot loader', () => {
+    // change the spinner type to dot
+    spinnerState.spinnerOptions.type = 'dot';
+    render(<SpinnerOverlay />);
+    expect(screen.getByLabelText('Loading')).toBeInTheDocument();
+  });
+
+  it('should not render any overlay if the showSpinner state is false', () => {
+    spinnerState.showSpinner = false;
+    render(<SpinnerOverlay />);
+    expect(screen.queryByLabelText('spinner-loader')).not.toBeInTheDocument();
+    expect(screen.queryByLabelText('Loading')).not.toBeInTheDocument();
+  });
+});
